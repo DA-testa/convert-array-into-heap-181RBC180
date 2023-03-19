fd153643e0ea156b0a9a9fc2d2585acef866679a
Georgijs Jefimovs <118355218+181RBC180@users.noreply.github.com>
2023-03-19 23:06:20 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -33,7 +33,7 @@ def main():
     #         n = file.readline()
     #         while data:
     #             data = list(map(int,file.readline().split()))
-    output(input())
+    print(input())
     n = int(input())
     data = list(map(int,input().split()))
     assert len(data) == n
